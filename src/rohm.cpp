3a5fa14ab9468c4a30775c10bd38e16fd941c336
Kirk Roerig <mr.possoms@gmail.com>
2020-07-25 08:32:20 -0600

    Added gcs to ecef conversion. Changed coord to a vector

--- src/rohm.cpp
+++ src/rohm.cpp
@@ -1,13 +1,41 @@
 #include "rohm.h"
 #include <tiffio.h>
+#include <math.h>
+
+struct vec3
+{
+	double x, y, z;
+};
+
+vec3 gcs_to_ecef_km(rohm::coord c)
+{
+	const auto EARTH_EQUATORIAL_RAD_KM = 6378.1370;
+	const auto EARTH_POLAR_RAD_KM = 6356.7520;
+
+	auto dtr = M_PI / 180.0;
+
+	const auto lat = dtr * c.lat();
+	const auto lng = dtr * c.lng();
+
+	const auto lat_cos = cos(lat);
+	const auto lat_sin = sin(lat);
+	const auto lng_cos = cos(lng);
+	const auto lng_sin = sin(lng);
+
+	return { 
+		EARTH_EQUATORIAL_RAD_KM * lng_cos * lat_cos,
+		EARTH_EQUATORIAL_RAD_KM * lng_sin * lat_cos,
+		EARTH_POLAR_RAD_KM * lat_sin
+	};
+}
 
 bool get_tile_idx(rohm::coord c, size_t& r_out, size_t& c_out)
 {
-	if (c.lat < -90 || c.lat > 90) { return false; }
-	if (c.lng < -180 || c.lng > 180) { return false; }
+	if (c.lat() < -90 || c.lat() > 90) { return false; }
+	if (c.lng() < -180 || c.lng() > 180) { return false; }
 
-	r_out = c.lat > 0 ? 0 : 1;
-	c_out = (c.lng + 180) / 90;
+	r_out = c.lat() > 0 ? 0 : 1;
+	c_out = (c.lng() + 180) / 90;
 
 	return true;	
 }
@@ -24,8 +52,39 @@ rohm::window get_tile_window(size_t r, size_t c)
 }
 
 
+bool coord_to_idx(TIFF* tif, rohm::window win, rohm::coord c, size_t& r_out, size_t& c_out)
+{
+	if (!win.contains(c)) { return false; }
+
+	uint32 w, h;
+	TIFFGetField(tif, TIFFTAG_IMAGEWIDTH, &w);
+	TIFFGetField(tif, TIFFTAG_IMAGELENGTH, &h);
+
+	auto c1 = win.corner_se - win.corner_nw;
+	c -= win.corner_nw;
+	c /= c1;
+
+	r_out = h * c.lat();
+	c_out = w * c.lng();
+
+	return true;
+}
+
+
+void get_window_res(TIFF* tif, rohm::window win, size_t& w_out, size_t& h_out)
+{
+	size_t nw_r, nw_c, se_r, se_c;
+
+	coord_to_idx(tif, win, win.corner_nw, nw_r, nw_c);
+	coord_to_idx(tif, win, win.corner_se, se_r, se_c);
+
+	w_out = se_c - nw_c;
+	h_out = se_r - nw_r;
+}
+
+
 void rohm::estimate(
-	size_t r, size_t c,
+	size_t map_r, size_t map_c,
 	float** energy_map_out,
 	estimate_params params)
 {
@@ -39,11 +98,18 @@ void rohm::estimate(
 	for (size_t i = 0; i < 4; i++)
 	{ // load tiles
 		char path[128];
-		size_t r = 0; c = 0;
+		size_t r = 0, c = 0;
 
 		get_tile_idx(params.win.corner(i), r, c);
 		snprintf(path, sizeof(path), "data/%s.tif", TILE_NAMES[r][c]);
 
 		tiles[r][c] = TIFFOpen(path, "r");
+	}
+
+finish:
+	for (size_t r = 2; r--;)
+	for (size_t c = 4; c--;)
+	{
+		TIFFClose(tiles[r][c]);
 	}
 }
--- src/rohm.h
+++ src/rohm.h
@@ -1,15 +1,98 @@
 #pragma once
 
 #include <stdlib.h>
+#include <initializer_list>
 
 namespace rohm
 {
 
-struct coord
+template <size_t N>
+struct vec
 {
-	double lat, lng;
+	vec() { memset(v, 0, sizeof(double) * N ); }
 
-	bool operator==(const coord& other) { return other.lat == lat && other.lng == lng; }
+	vec(std::initializer_list<double> init)
+	{
+		if (init.size() < N)
+		{
+			for (auto i = N; i--;)
+			{
+				v[i] = *init.begin();
+			}
+		}
+		else
+		{
+			auto i = 0;
+			for (auto s : init)
+			{
+				v[i++] = s;
+			}
+		}
+	}
+
+	bool operator==(const vec<N>& o)
+	{
+		for(int i = 0; i < N; i++)
+		{
+			if (o.v[i] != v[i]) { return false; } 
+		} 
+		return true;
+	}
+
+	vec<N> operator-(const vec<N>& o)
+	{
+		vec<N> out;
+		for (int i = 0; i < N; i++) { out.v[i] = v[i] - o.v[i]; }
+		return out;
+	}
+
+	vec<N> operator+(const vec<N>& o)
+	{
+		vec<N> out;
+		for (int i = 0; i < N; i++) { out.v[i] = v[i] + o.v[i]; }
+		return out;
+	}
+
+	vec<N> operator*(double s)
+	{
+		vec<N> out;
+		for (int i = 0; i < N; i++) { out.v[i] = v[i] * s; }
+		return out;
+	}
+
+	vec<N>& operator-=(const vec<N>& o)
+	{
+		for (int i = 0; i < N; i++) { v[i] -= o.v[i]; }
+		return *this;
+	}
+
+	vec<N> operator/=(const vec<N>& o)
+	{
+		for (int i = 0; i < N; i++) {  v[i] /= o.v[i]; }
+		return *this;
+	}
+
+	double v[N];
+};
+
+struct coord : public vec<2>
+{
+	coord () : vec<2>() {}
+	coord (double lat, double lng) : vec<2>({ lat, lng }) { }
+	// double lat, lng;
+
+	// bool operator==(const coord& o) { return o.lat == lat && o.lng == lng; }
+	// coord operator-(const coord& o) { return { lat - o.lat, lng - o.lng }; }
+	// coord operator+(const coord& o) { return { lat + o.lat, lng + o.lng }; }
+	// coord operator*(double s) { return { lat * s, lng * s }; }
+	// coord operator-=(const coord& o) { lat -= o.lat; lng -= o.lng; }
+	// coord operator/=(const coord& o) { lat /= o.lat; lng /= o.lng; }
+	
+	inline double lat() const { return v[0]; }
+	inline double lng() const { return v[1]; }
+
+	inline double lat(double l) { return v[0] = l; }
+	inline double lng(double l) { return v[1] = l; }
 };
 
 struct window
@@ -23,19 +106,25 @@ struct window
 			case 0:
 				return corner_nw;
 			case 1:
-				return { corner_nw.lat, corner_se.lng };
+				return { corner_nw.lat(), corner_se.lng() };
 			case 2:
 				return corner_se;
 			case 3:
-				return { corner_se.lat, corner_nw.lng };
+				return { corner_se.lat(), corner_nw.lng() };
 			default:
 				return {};
 		}
 	}
 
-	bool operator==(const window& other)
+	bool contains(const coord& c)
 	{
-		return corner_nw == other.corner_nw && corner_se == other.corner_se;
+		return corner_nw.lat() >= c.lat() && c.lat() >= corner_se.lat() &&
+		       corner_nw.lng() <= c.lng() && c.lng() <= corner_se.lng();
+	}
+
+	bool operator==(const window& o)
+	{
+		return corner_nw == o.corner_nw && corner_se == o.corner_se;
 	}
 };
 
@@ -49,6 +138,7 @@ struct vehicle_params
 struct estimate_params
 {
 	window win;
+	coord origin;
 	vehicle_params car;
 };
 
