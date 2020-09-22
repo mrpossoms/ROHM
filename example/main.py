b86b41b243a468f1417b0ad6b6f2dbb53a4d47a4
Kirk Roerig <kirk.roerig@advanced-space.com>
2020-09-21 21:51:37 -0600

    Working on webapp

---
+++ example/main.py
@@ -0,0 +1,27 @@
+import os
+
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app = Flask('ROHM', static_url_path='')
+
+
+@app.route("/")
+def index():
+    return render_template("main.html")
+
+
+if __name__ == '__main__':
+    port = 8080
+    if 'PORT' in os.environ:
+        port = os.environ['PORT']
+
+    try:
+        if os.environ['TEMPLATES_AUTO_RELOAD']:
+            app.config['TEMPLATES_AUTO_RELOAD'] = True
+    except KeyError:
+        print('Not reloading view templates')
+
+    app.run(port=port, host='0.0.0.0')
+
