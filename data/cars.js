42176c2258bcecc8d6007b4b775ae5131ee8e12a
Kirk Roerig <mr.possoms@gmail.com>
2020-10-12 09:05:28 -0600

    Working on UI and car database

---
+++ data/cars.js
@@ -0,0 +1,14 @@
+{
+	"Volkswagen e-Golf 2016": {
+		"energy_kwh": 24.4,
+		"mass_kg": 1536.36,
+		"avg_kwh_km": 5.95,
+        "regen_efficiency": 0.12,
+	},
+	"Tesla Model Y Long Range 2020": {
+		"energy_kwh": 72.5,
+		"mass_kg": 2405,
+		"avg_kwh_km": 5.84,
+        "regen_efficiency": 0.12,
+	}
+}
