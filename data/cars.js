bdae8964240f15c7f49035b767b2b958b3b1031f
Kirk Roerig <mr.possoms@gmail.com>
2020-10-12 20:15:46 -0600

    Working on car search

--- data/cars.js
+++ data/cars.js
@@ -3,12 +3,12 @@
 		"energy_kwh": 24.4,
 		"mass_kg": 1536.36,
 		"avg_kwh_km": 5.95,
-        "regen_efficiency": 0.12,
+        "regen_efficiency": 0.12
 	},
 	"Tesla Model Y Long Range 2020": {
 		"energy_kwh": 72.5,
 		"mass_kg": 2405,
 		"avg_kwh_km": 5.84,
-        "regen_efficiency": 0.12,
+        "regen_efficiency": 0.12
 	}
 }
