8057f1e08db8dfa320e697dc062eb253fa09b11e
Kirk Roerig <mr.possoms@gmail.com>
2020-11-20 20:45:43 -0700

    Working on auth and users

--- ui/urls.py
+++ ui/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
-
+from django.contrib.auth import views as auth_views
 from . import views
 
 
 urlpatterns = [
     path('', views.index, name="index"),
     path('modal/<str:modal_name>', views.modal, name="modal"),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html')),
 ]
